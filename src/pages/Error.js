a1a2fcf03d1d2cee1d49aac7f75cc5417c2325d2
WladyslawK <vladkurr7890@gmail.com>
2023-02-03 23:25:05 +0100

    feat(Error) Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
 const Error = () => {
-  return <h2>error page</h2>;
+    return (
+        <Wrapper>
+          <div>
+            <h1>404</h1>
+            <h3>Sorry, the page you tried cannot be found</h3>
+              <Link to={'/'} className='btn'>
+                  back home
+              </Link>
+          </div>
+        </Wrapper>
+    );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
@@ -10,9 +21,11 @@ const Wrapper = styled.section`
   place-items: center;
   background: var(--clr-primary-10);
   text-align: center;
+
   h1 {
     font-size: 10rem;
   }
+
   h3 {
     color: var(--clr-grey-3);
     margin-bottom: 1.5rem;
